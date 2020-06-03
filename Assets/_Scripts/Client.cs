65745ac805389eddf12d441978fd5688590f082d
axiomaticTwist <shanekim28@gmail.com>
2020-06-02 23:04:37 -0700

    Created networking branch
    Created TCP client connection to game server and simple UI

---
+++ Assets/_Scripts/Client.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+public class Client : MonoBehaviour {
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+
+    public string ip = "127.0.0.1";
+    public int port = 27015;
+    public int myId = 0;
+    public TCP tcp;
+
+    // Start is called before the first frame update
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Debug.Log("Instance already exists, destroying objects");
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Start() {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer() {
+        tcp.Connect();
+	}
+
+    public class TCP {
+        public TcpClient socket;
+
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+
+        public void Connect() {
+            socket = new TcpClient {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+        }
+
+        private void ConnectCallback(IAsyncResult result) {
+            socket.EndConnect(result);
+
+            if (!socket.Connected) {
+                return;
+            }
+
+            stream = socket.GetStream();
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult result) {
+            try {
+                int byteLength = stream.EndRead(result);
+                if (byteLength <= 0) {
+                    return;
+                }
+
+                byte[] data = new byte[byteLength];
+                Array.Copy(receiveBuffer, data, byteLength);
+
+                // TODO: Handle data
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            } catch (Exception e) {
+                Console.WriteLine("Error receiving TCP data: " + e.ToString());
+                // TODO: Disconnect
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
+using System.Security.Policy;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	public static UIManager instance;
+
+	public GameObject startMenu;
+	public InputField usernameField;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Debug.Log("Instance already exists, destroying objects");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer() {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
+	}
+}
