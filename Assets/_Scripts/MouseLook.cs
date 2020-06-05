5cf468f17b585bd8489be0433ab6db0137100a6d
JarodB <39526954+Jarod-B@users.noreply.github.com>
2020-06-05 01:16:22 -0700

    Added basic movement, wallrunning, and grappling

---
+++ Assets/_Scripts/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	float mouseSensitivity = 150f;
+
+	public Transform playerBody;
+	public PlayerMovement movement;
+	PlayerMovement.PlayerState currentPlayerState;
+
+	float xRotation = 0f;
+	float yRotation = 0f;
+
+	float lerpSpeed = 10f;
+
+	void Start() {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update() {
+		currentPlayerState = movement.currentState;
+
+		// Get mouse input values
+
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		// Clamp X rotation
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		// Apply rotation based on mouse movement
+		// TODO - this monstrosity absolutely destroys performance. should fix.
+		if (currentPlayerState == PlayerMovement.PlayerState.WallrunLeft) {
+			yRotation += mouseX;
+			yRotation = Mathf.Clamp(yRotation, -15f, 45f);
+			transform.localRotation = Quaternion.Slerp(Quaternion.Euler(xRotation, yRotation, transform.localEulerAngles.z), Quaternion.Euler(xRotation, yRotation, -10f), Time.deltaTime * lerpSpeed);
+		} else if (currentPlayerState == PlayerMovement.PlayerState.WallrunRight) {
+			yRotation += mouseX;
+			yRotation = Mathf.Clamp(yRotation, -45f, 15f);
+			transform.localRotation = Quaternion.Slerp(Quaternion.Euler(xRotation, yRotation, transform.localEulerAngles.z), Quaternion.Euler(xRotation, yRotation, 10f), Time.deltaTime * lerpSpeed);
+		} else {
+			yRotation = 0;
+			transform.localRotation = Quaternion.Slerp(Quaternion.Euler(xRotation, transform.localEulerAngles.y, transform.localEulerAngles.z), Quaternion.Euler(xRotation, 0f, 0f), Time.deltaTime * lerpSpeed);
+			playerBody.Rotate(Vector3.up * mouseX);
+		}
+		// brb, gotta go throw up
+	}
+}
---
+++ Assets/_Scripts/RotateGun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour {
+	[SerializeField] GrapplingGun grapple;
+
+	Quaternion desiredRotation;
+	float rotationSpeed = 5f;
+
+	void Update() {
+		if (!grapple.IsGrappling()) {
+			desiredRotation = transform.parent.rotation;
+		} else {
+			desiredRotation = Quaternion.LookRotation(grapple.GetGrapplePoint() - transform.position);
+		}
+
+		transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+	}
+}
